a855e652d2822214bcd137c0b71026915457eb4f
Cuong Hoang~ <hoangcuongbk80@gmail.com>
2024-01-19 11:38:49 +0700

    Lecture 1

---
+++ Lecture1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comp1551_Demo
+{
+    internal class Lecture1
+    {
+        static public double SquareRoot(double Result)
+        {
+            return (Math.Sqrt(Result));
+        }
+        static public void Task1()
+        {
+            Console.WriteLine("----Task 1-------");
+            Console.WriteLine(SquareRoot(16));
+        }
+        static public void Task2()
+        {
+            Console.WriteLine("----Task 2-------");
+            Console.Write("Enter a number: ");
+            double num = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(SquareRoot(num));
+        }
+        static public void Task3()
+        {
+            Console.WriteLine("----Task 3-------");
+            Console.Write("Enter a number: ");
+            double num = Convert.ToDouble(Console.ReadLine());
+            if(num >= 0)
+            {
+                Console.WriteLine(SquareRoot(num));
+            }
+            else
+            {
+                Console.WriteLine("Negative Number!");
+            }
+        }
+
+        static public void QuadraticEquation(double a, double b, double c)
+        {
+            double x1, x2, D;
+            D = Convert.ToDouble(b * b - 4 * a * c);
+            if ((a != 0) & (D >= 0))
+            {
+                x1 = Convert.ToDouble((-b + Math.Sqrt(D)) / (2 * a));
+                x2 = Convert.ToDouble((-b - Math.Sqrt(D)) / (2 * a));
+                Console.WriteLine("The result is: {0} and {1}", x1, x2);
+            }
+            else
+            {
+                Console.WriteLine("No solutions");
+            }
+        }
+        static public void Task4()
+        {
+            Console.WriteLine("----Task 4-------");
+            double v1, v2, v3;
+            Console.WriteLine("Enter values for a, b and c:");
+            v1 = Convert.ToDouble(Console.ReadLine());
+            v2 = Convert.ToDouble(Console.ReadLine());
+            v3 = Convert.ToDouble(Console.ReadLine());
+            QuadraticEquation(v1, v2, v3);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Comp1551_Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Lecture1.Task1();
+            Lecture1.Task2();
+            Lecture1.Task3();
+            Lecture1.Task4();
+        }
+    }
+}
